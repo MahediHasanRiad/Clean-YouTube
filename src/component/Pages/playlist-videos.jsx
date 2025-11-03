5c973e2d3b30302c8bea4889815db3ef9d220b21
Mahedi Hasan Riad <contact.mahedihasan@gmail.com>
2025-11-04 01:07:29 +0600

    router

--- src/component/Pages/playlist-videos.jsx
+++ src/component/Pages/playlist-videos.jsx
@@ -1,13 +1,35 @@
 import { useParams } from 'react-router-dom'
+import usePlaylistItem from '../Hooks/usePlaylist'
+import { useEffect } from 'react'
 
-function PlaylistAllVideos({ playlists }) {
+function PlaylistAllVideos() {
 
-  const playlistId = useParams()
+  const { id: playlistId } = useParams()
+  const { playlists, getPlaylistById, error, loading } = usePlaylistItem()
 
+  useEffect(() => {
+    getPlaylistById(playlistId)
+  }, [playlistId])
+
+  // find --> Playlist
+  const playlist = playlists[playlistId]
+
+  if (!playlist) {
+    return <div className="container mt-5"><h3>Playlist not found</h3></div>
+  }
+
+  if(error){
+    return <div className="container mt-5"><h3>Error: {error}</h3></div>
+  }
+
+  if(loading){
+    return <div className="container mt-5"><h3>Loading...</h3></div>
+  }
 
   return (
     <div>
-      <h1>Playlist Video</h1>
+      <h1>{playlist.channelTitle}</h1>
+      <p>{playlist.playlistDescription}</p>
     </div>
   )
 }
